81a4d5395e93b63862dad610ab90eba897587c1c
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2022-11-01 04:17:23 +0900

    minimax

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import './App.css'
-import Game from "./pages/Game"
-import useTickTackToe from "./hooks/useTicTacToe"
+import Game from "@/pages/Game"
+import useTickTackToe from "@/hooks/useTicTacToe"
 import * as O from 'fp-ts/Option'
 
 function App() {
-  const game = useTickTackToe()
+  const game = useTickTackToe(false, true)
   return (
     <div className='App'> 
       <h1>Tic Tac Toe</h1>
-      <Game board={game.board} handleClick={game.handleClick} /> 
+      <Game board={game.state.board} handleClick={game.handleClick} /> 
       <h2>
         {   
-          game.status !== 'finished' 
-            ? <>turn : {game.turn}</>
+          game.state.status !== 'finished' 
+            ? <>turn : {game.state.turn}</>
             : O.match (
                 () => "draw",
                 (side) => "winner : " + side,
-              ) (game.winner) 
+              ) (game.state.winner) 
         } 
       </h2>
       {   
-        game.status === 'finished' 
+        game.state.status === 'finished' 
           ? <button onClick={game.handleInit}>Restart</button> 
           : <></>
       } 
--- src/hooks/useTicTacToe.ts
+++ src/hooks/useTicTacToe.ts
@@ -1,72 +1,200 @@
 import { useEffect, useState } from 'react' 
-import * as O from 'fp-ts/Option'
+import * as O from 'fp-ts/Option' 
 import * as A from 'fp-ts/Array'
-import * as F from 'fp-ts/function'
+import * as F from 'fp-ts/function' 
 import { Side } from '@/types/Side'
-import { GameStatus } from '@/types/GameStatus'
+import { GameStatus } from '@/types/GameStatus'  
 
-const isFinished = (board: O.Option<Side>[]) => (turn : Side) =>
-    F.pipe (
-        [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-            [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
-        ] ,  
-        A.exists(positions => 
-            positions
-                .map(position => board[position]) 
-                .every(value => O.isSome(value) && value.value === turn)   
-        ),
-        (existWinner) => 
-            existWinner 
-            ? O.some(O.some(turn)) 
-            : board.every(O.isSome) 
-                ? O.some(O.none)
-                : O.none
-    )  
+type Board = O.Option<Side>[]
 
-export default (): {
-    board: O.Option<Side>[]
-    status: string
+module Board {
+    export const set = (index: number) => (side: Side) => (board: Board) => { 
+        const nextBoard = [...board] // deep copy
+        nextBoard[index] = O.some(side)
+        return nextBoard
+    }
+    export const getSelectableIndices = (board: Board) =>
+        F.pipe (
+            board,
+            A.mapWithIndex ((i, v) => 
+                F.pipe(
+                    v,
+                    O.match(
+                        () => O.some (i),
+                        (_) => O.none
+                    )
+                )
+            ),
+            A.filter (O.isSome),
+            A.map((i) => i.value)
+        )
+    export const isFinished = (board: O.Option<Side>[]) =>
+        F.pipe (
+            [
+                [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+            ] ,  
+            A.map(A.map((i) => board[i])),
+            A.filter(A.every(O.isSome)),
+            A.map(A.map((o) => o.value)),
+            A.findFirstMap((sides)  => {             
+                if (sides.every((s) => s === sides[0] ))  
+                    return O.some(sides[0]) 
+                else  
+                    return O.none 
+            }), 
+            O.match(
+                () => board.every(O.isSome) ? O.some(O.none) : O.none,
+                F.flow (O.some , O.some)
+            )
+        )  
+}
+
+type GameState = {
+    board: Board
+    status: GameStatus
     turn: Side
-    winner: O.Option<string>
+    winner: O.Option<Side>
+} 
+ 
+module CPU {
+    export const evaluateBoard = (turn: Side) => (board: Board) => 
+        F.pipe(
+            Board.isFinished(board),
+            O.match(
+                () => 0.,   
+                O.match(
+                    () => 0.0, 
+                    (winner) => winner === turn ? 1.0 : -1.0
+                ) 
+            )
+        )  
+
+    export const minimax =
+        (evaluate : (board: Board) => number) =>
+        (isMaximizing: boolean) =>  
+        (depth: number) => 
+        (turn: Side) => 
+        (board: Board) : [O.Option<number>, number] => {
+
+        if (depth === 0 || O.isSome(Board.isFinished(board))) { 
+            return [O.none, evaluate (board)]
+        }
+        return (
+            F.pipe(
+                board,
+                Board.getSelectableIndices, 
+                A.map<number, [O.Option<number>, number]>((index) => {
+                    const nextBoard = Board.set(index)(turn)(board)
+                    const [_, ret] = 
+                        minimax (evaluate) (!isMaximizing) (depth - 1) (turn === 'O' ? 'X' : 'O') (nextBoard)
+                    return [O.some (index), ret] 
+                }),   
+                A.reduce<[O.Option<number>, number], [O.Option<number>, number]>(
+                    [O.none, isMaximizing ? -Infinity : Infinity],
+                    ([accI, acc], [xi, x]) => (
+                        isMaximizing 
+                            ? acc < x ? [xi, x] : [accI, acc] 
+                            : x < acc ? [xi, x] : [accI, acc]
+                    )
+                ) 
+            ) 
+        ) 
+    }    
+
+    export const randomSelect = (board: Board) => { 
+        const candidates = Board.getSelectableIndices (board) 
+        return ( 
+            (candidates.length > 0)
+            ? O.some (candidates[Math.floor(Math.random() * candidates.length)])
+            : O.none 
+        )
+    }
+
+}
+
+ 
+
+export default (XisCPU : boolean, OisCPU : boolean): {
+    state: GameState
     handleClick: (index: number) => void
     handleInit: () => void
 } => { 
-    const [board, setBoard] = useState<O.Option<Side>[]>(Array(9).fill(O.none))
-    const [turn, setTurn] = useState<Side>('X')
-    const [winner, setWinner] = useState<O.Option<Side>>(O.none) 
-    const [status, setStatus] = useState<GameStatus>('playing') 
- 
-    const handleClick = (n: number) : void => {  
-        if (0 <= n && n < 9  
-            && O.isNone(board[n])  
-            && status == 'playing') { 
 
-            const nextBoard = [...board] // deep copy
-            nextBoard[n] = O.some(turn)
-            F.pipe (
-                isFinished (nextBoard) (turn),
+    const initGameState : GameState = {
+        board: Array(9).fill(O.none),
+        status: 'playing',
+        turn: 'X',
+        winner: O.none
+    }
+    
+    const [state, setState] = useState<GameState>(initGameState)
+
+    const updateState = (index: number) => { 
+        const nextBoard = Board.set (index) (state.turn) (state.board)
+        F.pipe (
+            Board.isFinished (nextBoard) ,
+            O.match(
+                () => {   
+                    setState(
+                        {
+                            board: nextBoard,
+                            turn: state.turn == 'X' ? 'O' : 'X',
+                            status: 'playing',
+                            winner: O.none
+                        }
+                    )
+                }, 
+                (result) => {  
+                    setState(
+                        {
+                            board: nextBoard,
+                            turn: state.turn == 'X' ? 'O' : 'X',
+                            status: 'finished',
+                            winner: result
+                        }
+                    ) 
+                }
+            )
+        ) 
+    } 
+ 
+    useEffect(() => {  
+        if (state.status === 'playing' 
+            && ((state.turn == 'X' && XisCPU) || (state.turn == 'O' && OisCPU))) {  
+            
+            F.pipe(
+                CPU.minimax (CPU.evaluateBoard (state.turn)) (true) (9) (state.turn) (state.board) ,
+                ([move, _]) => move,
                 O.match(
-                    () => { 
-                        setTurn(turn == 'X' ? 'O' : 'X') 
-                    }, 
-                    (result) => { 
-                        setStatus('finished')
-                        setWinner(result) 
-                    }
-                )
-            ) 
-            setBoard(nextBoard)
+                    () => {},
+                    (index) => updateState(index) 
+                ) 
+            )
+
+            // F.pipe(
+            //     CPU.randomSelect (state.board),
+            //     O.match(
+            //         () => {},
+            //         (index) => updateState(index)
+            //     ) 
+            // )
+            
+        }
+    }, [state])
+  
+    const handleClick = (n: number) : void => {  
+        if (
+            O.isNone(state.board[n]) 
+            && state.status == 'playing'
+            && ((state.turn == 'X' && !XisCPU) || (state.turn == 'O' && !OisCPU)) 
+            ) {
+            updateState(n)
         }
     }
  
     const handleInit = () : void => { 
-        setBoard(Array(9).fill(O.none))
-        setWinner(O.none)
-        setTurn('X')
-        setStatus('playing')
+        setState(initGameState)
     }
- 
 
-    return { board, status, turn, winner, handleClick, handleInit };
+    return { state, handleClick, handleInit };
 }
