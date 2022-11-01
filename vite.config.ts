530c8f6c83305d289bfd9eaf28e3e4a3fb521721
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2022-11-01 13:45:17 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
+// https://chaika.hatenablog.com/entry/2022/05/14/083000
+export default defineConfig({ 
+  build: {
+    // root (= ./src) から見た相対パスで指定
+    outDir: './dist',
+  },
   server: {
     open: true,
   },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '@': 'src'  
